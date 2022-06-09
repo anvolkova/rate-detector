500650ba9ced27b4e5e98147cd9caafc2215bfff
Anna Volkova <annettavo@gmail.com>
2022-06-09 12:52:28 +1000

    Add error message

--- rate_detector.rb
+++ rate_detector.rb
@@ -3,10 +3,14 @@ require './csv_writer.rb'
 require './rate_counter.rb'
 
 # Reading input parameters.
-input_arr = ARGV
-csv_file = input_arr[0]
-rate_of_change = input_arr[1].to_i
+csv_file = ARGV[0]
+rate_of_change = ARGV[1].to_i
 
-readings = CsvReader.new.read(csv_file)
+begin
+    readings = CsvReader.new.read(csv_file)
+rescue Errno::ENOENT
+    puts "File #{csv_file} not found"
+    return
+end
 intervals = RateCounter.new.solve(readings, rate_of_change)
 puts CsvWriter.new.write(intervals)
